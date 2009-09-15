44688df09ca10b483363514cf1a980b2f86f851f
jeffrey.blattman <jeffrey.blattman@a2ce84bc-982a-11de-b8e1-0bcae535652c>
2009-09-15 00:08:27 +0000

    1.0.6

--- trunk/src/org/jtb/csc/CSVParser.java
+++ trunk/src/org/jtb/csc/CSVParser.java
@@ -12,44 +12,27 @@ class CSVParser {
 		}
 
 		StringBuilder sb = new StringBuilder();
-		boolean quotes = false;
-		boolean leading = true;
 		for (int i = 0; i < line.length(); i++) {
 			char c = line.charAt(i);
 			switch (c) {
-			case '"':
-				quotes = !quotes;
-				leading = false;
-				break;
 			case ',':
 			case '|':
-				if (!quotes) {
-					tokens.add(sb.toString());
-					sb = new StringBuilder();
-					leading = true;
-				}
+				tokens.add(sb.toString());
+				sb = new StringBuilder();
 				break;
 			case '\n':
 			case '[':
 			case ']':
 			case '(':
 			case ')':
-				break;
-			case ' ':
+			case '"':
 			case '\t':
-				if (!leading) {
-					sb.append(c);
-				}
 				break;
 			default:
-				leading = false;
 				sb.append(c);
 			}
-			if (i == line.length() - 1) {
-				tokens.add(sb.toString());
-			}
 		}
-
+		tokens.add(sb.toString());
 		return tokens;
 
 	}
